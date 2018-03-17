a08ee7c288861982850f3f22b74749e604857fcc
Richard Gordon <richgordon95@gmail.com>
2018-03-17 17:42:30 -0400

    added initial ajax call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
-
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/rgordon95.json',
+    dateType: 'jsonp',
+    success: function(response) {
+      //convert json to js
+      //create html to hold response
+      createElement('DIV')
+      .addClass('.course')
+      .appendTo('#badges')
+      //insert data from json or js object into html
+      //append html to page
+    }
+  });
 
 });
+
+// https://www.codeschool.com/users/rgordon95.json
